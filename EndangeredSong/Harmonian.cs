52223d9665fbd19a30e91d68e6439f1b19ca90ff
AmberJMLee <alphagirl93@yahoo.com>
2015-05-22 12:53:52 -0700

    Initial Commit

---
+++ EndangeredSong/Harmonian.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Harmonian
+{
+    string musicFile;
+    string imageFile;
+    int X;
+    int Y;
+	public Harmonian()
+	{
+        musicFile = "";
+        imageFile = "";
+	}
+    public Harmonian(string mF, string iF)
+    {
+        musicFile = mF;
+        imageFile = iF;
+    }
+}
